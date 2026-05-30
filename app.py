6fc9ba13b05e8369ab58a819f77b35e7d956433b
pankajsyadav <163241835+pankajsyadav@users.noreply.github.com>
2026-05-30 03:43:56 +0000

    added code to install missing libraries

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+import subprocess
+import sys
+
+subprocess.check_call(
+	[sys.executable, "-m", "pip", "install", "-q", "scikit-learn", "streamlit", "pandas", "numpy", "ipykernel"]
+)
+subprocess.check_call(
+	[sys.executable, "-m", "pip", "install", "ipykernel", "-U", "--user", "--force-reinstall"]
+)
 import os
 import re
 import zipfile
