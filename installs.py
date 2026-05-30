ff1f0a794dd99d03effd951049e67e1c919da90d
pankajsyadav <163241835+pankajsyadav@users.noreply.github.com>
2026-05-30 03:40:19 +0000

    updated infor

--- installs.py
+++ installs.py
@@ -2,5 +2,8 @@ import subprocess
 import sys
 
 subprocess.check_call(
-	[sys.executable, "-m", "pip", "install", "-q", "scikit-learn", "streamlit", "pandas", "numpy"]
+	[sys.executable, "-m", "pip", "install", "-q", "scikit-learn", "streamlit", "pandas", "numpy", "ipykernel"]
+)
+subprocess.check_call(
+	[sys.executable, "-m", "pip", "install", "ipykernel", "-U", "--user", "--force-reinstall"]
 )
